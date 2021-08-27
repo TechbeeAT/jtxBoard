6437737146ea441b93980c174af07ae1af364a4e
Patrick Lang <patrick.lang85@gmail.com>
2021-08-27 15:04:14 +0200

    Set dirty = false for initialization of ICalObject to avoid double-sync at sync-process

--- app/src/main/java/at/bitfire/notesx5/database/ICalObject.kt
+++ app/src/main/java/at/bitfire/notesx5/database/ICalObject.kt
@@ -366,7 +366,7 @@ data class ICalObject(
 
     @ColumnInfo(index = true, name = COLUMN_ICALOBJECT_COLLECTIONID) var collectionId: Long = 1L,
 
-    @ColumnInfo(name = COLUMN_DIRTY) var dirty: Boolean = true,
+    @ColumnInfo(name = COLUMN_DIRTY) var dirty: Boolean = false,
     @ColumnInfo(name = COLUMN_DELETED) var deleted: Boolean = false,
 
     @ColumnInfo(name = COLUMN_FILENAME)   var fileName: String? = null,
