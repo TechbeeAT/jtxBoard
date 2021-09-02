f79f1b66d0423347590dfaaba4e594f246e203c7
Patrick Lang <patrick.lang85@gmail.com>
2021-09-02 21:52:59 +0200

    Added String resources for settings Added Espresso Testcases

--- app/src/androidTest/java/at/bitfire/notesx5/EspressoTestcase.kt
+++ app/src/androidTest/java/at/bitfire/notesx5/EspressoTestcase.kt
@@ -12,8 +12,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -27,7 +26,7 @@ import org.junit.runner.RunWith
 class EspressoTestcase {
 
         /**
-         * Use [ActivityScenarioRule] to create and launch the activity under test before each test,
+         * Use [androidx.test.ext.junit.rules.ActivityScenarioRule] to create and launch the activity under test before each test,
          * and close it after each test. This is a replacement for
          * [androidx.test.rule.ActivityTestRule].
          */
@@ -75,4 +74,27 @@ class EspressoTestcase {
         onView(withId(R.id.edit_fab_save)).perform(click())
         onView(withText("Espresso Todo"))
     }
+
+
+    @Test
+    fun Open_about_click_through_tabs() {
+
+        onView(withContentDescription("Open navigation drawer")).perform(click())
+        onView(withId(R.id.nav_about)).perform(click())
+        onView(withContentDescription("Translations")).perform(click())
+        onView(withContentDescription("Libraries")).perform(click())
+        onView(withContentDescription("NOTESx5")).perform(click())
+
+        onView(withId(R.id.about_notesx5_icon))
+    }
+
+    @Test
+    fun Open_settings_menu() {
+
+        onView(withContentDescription("Open navigation drawer")).perform(click())
+        onView(withId(R.id.nav_app_settings)).perform(click())
+        onView(withText(R.string.settings_enforce_dark_theme))
+
+    }
+
 }
