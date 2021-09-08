8c987c4adc924ff8d5f9bd2a9efe4c23403f37e2
Patrick Lang <patrick.lang85@gmail.com>
2021-09-08 21:54:18 +0200

    Fixed Test

--- app/src/androidTest/java/at/techbee/jtx/EspressoTestcase.kt
+++ app/src/androidTest/java/at/techbee/jtx/EspressoTestcase.kt
@@ -81,9 +81,9 @@ class EspressoTestcase {
 
         onView(withContentDescription("Open navigation drawer")).perform(click())
         onView(withId(R.id.nav_about)).perform(click())
-        onView(withContentDescription("Translations")).perform(click())
-        onView(withContentDescription("Libraries")).perform(click())
-        onView(withContentDescription("JTX")).perform(click())
+        onView(withContentDescription(R.string.about_tabitem_translations)).perform(click())
+        onView(withContentDescription(R.string.about_tabitem_libraries)).perform(click())
+        onView(withContentDescription(R.string.about_tabitem_jtx)).perform(click())
 
         onView(withId(R.id.about_app_icon))
     }
