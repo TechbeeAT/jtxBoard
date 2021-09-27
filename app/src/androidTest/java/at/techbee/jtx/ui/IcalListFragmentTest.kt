da3a4592084840eb7dddb7a8fd59d2d5bab5ba8c
Patrick Lang <patrick.lang85@gmail.com>
2021-09-27 18:28:22 +0200

    Extended tests

--- app/src/androidTest/java/at/techbee/jtx/ui/IcalListFragmentTest.kt
+++ app/src/androidTest/java/at/techbee/jtx/ui/IcalListFragmentTest.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -14,6 +15,9 @@ import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry
 import at.techbee.jtx.*
 import at.techbee.jtx.database.*
+import at.techbee.jtx.database.properties.Attachment
+import at.techbee.jtx.database.properties.Attendee
+import at.techbee.jtx.database.properties.Comment
 import kotlinx.coroutines.*
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.TestCoroutineScope
@@ -63,9 +67,30 @@ class IcalListFragmentTest {
         //insert sample entries
         testScope.launch(TestCoroutineDispatcher()) {
             database.insertCollectionSync(sampleCollection)
-            database.insertICalObjectSync(sampleJournal)
-            database.insertICalObjectSync(sampleNote)
-            database.insertICalObjectSync(sampleTodo)
+            val journalId = database.insertICalObjectSync(sampleJournal)
+            val noteId = database.insertICalObjectSync(sampleNote)
+            val taskId = database.insertICalObjectSync(sampleTodo)
+
+            //Attendees
+            database.insertAttendee(Attendee(icalObjectId = journalId, caladdress = "contact@techbee.at"))
+            database.insertAttendee(Attendee(icalObjectId = noteId, caladdress = "contact@techbee.at"))
+            database.insertAttendee(Attendee(icalObjectId = taskId, caladdress = "contact@techbee.at"))
+
+            //Attachments
+            database.insertAttachment(Attachment(icalObjectId = journalId, uri = "https://techbee.at"))
+            database.insertAttachment(Attachment(icalObjectId = journalId, uri = "https://jtx.techbee.at"))
+            database.insertAttachment(Attachment(icalObjectId = noteId, uri = "https://techbee.at"))
+            database.insertAttachment(Attachment(icalObjectId = taskId, uri = "https://techbee.at"))
+
+            //Comments
+            database.insertComment(Comment(icalObjectId = journalId, text = "my comment"))
+            database.insertComment(Comment(icalObjectId = journalId, text = "my comment2"))
+            database.insertComment(Comment(icalObjectId = journalId, text = "my comment3"))
+            database.insertComment(Comment(icalObjectId = noteId, text = "my comment"))
+            database.insertComment(Comment(icalObjectId = taskId, text = "my comment"))
+
+
+
 
             //ICalDatabase.getInstance(context).populateTestData()
         }
@@ -92,6 +117,13 @@ class IcalListFragmentTest {
         onView(withText(convertLongToTimeString(sampleJournal.dtstart))).check(matches(isDisplayed()))
         onView(withId(R.id.list_item_status)).check(matches(withText(R.string.journal_status_final)))
         onView(withId(R.id.list_item_classification)).check(matches(withText(R.string.classification_public)))
+        onView(withId(R.id.list_item_num_attendees_icon)).check(matches(isDisplayed()))
+        onView(withId(R.id.list_item_num_attendees_text)).check(matches(withText("1")))
+        onView(withId(R.id.list_item_num_attachments_icon)).check(matches(isDisplayed()))
+        onView(withId(R.id.list_item_num_attachments_text)).check(matches(withText("2")))
+        onView(withId(R.id.list_item_num_comments_icon)).check(matches(isDisplayed()))
+        onView(withId(R.id.list_item_num_comments_text)).check(matches(withText("3")))
+
     }
 
     @Test
@@ -102,6 +134,23 @@ class IcalListFragmentTest {
         onView(withText(R.string.list_tabitem_notes)).perform(click())
         onView(withText(sampleNote.summary)).check(matches(isDisplayed()))
         onView(withText(sampleNote.description)).check(matches(isDisplayed()))
+
+        onView(withText(convertLongToDayString(sampleJournal.dtstart))).check(doesNotExist())
+        onView(withText(convertLongToMonthString(sampleJournal.dtstart))).check(doesNotExist())
+        onView(withText(convertLongToYearString(sampleJournal.dtstart))).check(doesNotExist())
+        onView(withText(convertLongToTimeString(sampleJournal.dtstart))).check(doesNotExist())
+
+        onView(withId(R.id.list_item_status)).check(matches(withEffectiveVisibility(Visibility.GONE)))
+        onView(withId(R.id.list_item_classification)).check(matches(withEffectiveVisibility(Visibility.GONE)))
+        /*
+        onView(withId(R.id.list_item_num_attendees_icon)).check(matches(withEffectiveVisibility(Visibility.GONE)))
+        onView(withId(R.id.list_item_num_attendees_text)).check(matches(withEffectiveVisibility(Visibility.GONE)))
+        onView(withId(R.id.list_item_num_attachments_icon)).check(matches(withEffectiveVisibility(Visibility.GONE)))
+        onView(withId(R.id.list_item_num_attachments_text)).check(matches(withEffectiveVisibility(Visibility.GONE)))
+        onView(withId(R.id.list_item_num_comments_icon)).check(matches(withEffectiveVisibility(Visibility.GONE)))
+        onView(withId(R.id.list_item_num_comments_text)).check(matches(withEffectiveVisibility(Visibility.GONE)))
+
+         */
     }
 
     @Test
@@ -112,5 +161,10 @@ class IcalListFragmentTest {
         onView(withText(R.string.list_tabitem_todos)).perform(click())
         onView(withText(sampleTodo.summary)).check(matches(isDisplayed()))
         onView(withText(sampleTodo.description)).check(matches(isDisplayed()))
+        onView(withId(R.id.list_item_progress_label)).check(matches(isDisplayed()))
+        onView(withId(R.id.list_item_progress_checkbox)).check(matches(isDisplayed()))
+        onView(withId(R.id.list_item_progress_slider)).check(matches(isDisplayed()))
+        onView(withId(R.id.list_item_progress_percent)).check(matches(isDisplayed()))
+
     }
 }
