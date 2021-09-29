5301ea824dda9fc4040017661c84dfe0178aa936
Patrick Lang <patrick.lang85@gmail.com>
2021-09-29 13:45:23 +0200

    [WIP] Show another icon when the item is an exception to a recurring event

--- app/src/main/java/at/techbee/jtx/ui/IcalListAdapter.kt
+++ app/src/main/java/at/techbee/jtx/ui/IcalListAdapter.kt
@@ -276,8 +276,13 @@ class IcalListAdapter(var context: Context, var model: IcalListViewModel) :
             }
 
             //recur icon and text
-            if(iCal4ListItem.property.isRecurringInstance || iCal4ListItem.property.isRecurringOriginal)
+            if(iCal4ListItem.property.isRecurringInstance || iCal4ListItem.property.isRecurringOriginal) {
+                holder.recurIcon.setImageResource(R.drawable.ic_recurring)
                 holder.recurIcon.visibility = View.VISIBLE
+            } else if (iCal4ListItem.property.isRecurringException) {
+                holder.recurIcon.setImageResource(R.drawable.ic_recur_exception)
+                holder.recurIcon.visibility = View.VISIBLE
+            }
              else
                 holder.recurIcon.visibility = View.GONE
 
