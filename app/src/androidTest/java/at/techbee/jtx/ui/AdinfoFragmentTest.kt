e642b44122ec7f51ee5e5e272d34879b621698e8
Patrick Lang <patrick.lang85@gmail.com>
2021-11-17 19:38:00 +0100

    minor test fix

--- app/src/androidTest/java/at/techbee/jtx/ui/AdinfoFragmentTest.kt
+++ app/src/androidTest/java/at/techbee/jtx/ui/AdinfoFragmentTest.kt
@@ -3,7 +3,6 @@ package at.techbee.jtx.ui
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
