205fe5e6326c09dc67322ef6414ab575d34b7eca
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2022-05-27 12:14:57 +0200

    Fix crash when deleting hierarchical entries from a local collection Closes #211

--- app/src/main/java/at/techbee/jtx/ui/IcalViewFragment.kt
+++ app/src/main/java/at/techbee/jtx/ui/IcalViewFragment.kt
@@ -192,13 +192,9 @@ class IcalViewFragment : Fragment() {
         icalViewViewModel.icalEntity.observe(viewLifecycleOwner) {
 
             if (it == null) {
-                Toast.makeText(
-                    context,
-                    R.string.view_toast_entry_does_not_exist_anymore,
-                    Toast.LENGTH_LONG
-                ).show()
-                view?.findNavController()
-                    ?.navigate(IcalViewFragmentDirections.actionIcalViewFragmentToIcalListFragment())
+                if(summary2delete.isEmpty())
+                    Toast.makeText(context, R.string.view_toast_entry_does_not_exist_anymore, Toast.LENGTH_LONG).show()
+                findNavController().navigate(NavigationDirections.actionGlobalIcalListFragment())
                 return@observe   // just make sure that nothing else happens
             }
 
