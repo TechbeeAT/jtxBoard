055df607161d3034550a669db906a522cc111655
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2022-06-02 11:32:52 +0200

    Fixed wrong status bar color when using compose in list

--- app/src/main/java/at/techbee/jtx/ui/theme/Theme.kt
+++ app/src/main/java/at/techbee/jtx/ui/theme/Theme.kt
@@ -1,19 +1,10 @@
 package at.techbee.jtx.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = md_theme_light_primary,
@@ -61,6 +52,7 @@ fun JtxBoardTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+    /*
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
@@ -68,6 +60,7 @@ fun JtxBoardTheme(
             ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
         }
     }
+     */
 
     MaterialTheme(
         colorScheme = colorScheme,
