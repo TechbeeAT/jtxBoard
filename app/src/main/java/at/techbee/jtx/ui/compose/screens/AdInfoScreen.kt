e55142c9bc7ab1ce42da2bfd9c2e0c1dace439fa
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2022-06-19 11:10:45 +0200

    Code cleanup

--- app/src/main/java/at/techbee/jtx/ui/compose/screens/AdInfoScreen.kt
+++ app/src/main/java/at/techbee/jtx/ui/compose/screens/AdInfoScreen.kt
@@ -8,24 +8,17 @@
 
 package at.techbee.jtx.ui.compose.screens
 
-import android.content.Intent
-import android.net.Uri
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,11 +27,9 @@ import at.techbee.jtx.ui.theme.JtxBoardTheme
 import at.techbee.jtx.ui.theme.Typography
 
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun AdInfoScreen(modifier: Modifier = Modifier) {
 
-    val context = LocalContext.current
     val scrollState = rememberScrollState()
 
     Column(
