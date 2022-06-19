7f15f66ab7547d72704d0cbe450eedded3f1b8b6
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2022-06-19 17:57:58 +0200

    Apply theme in about libraries Closes #263

--- app/src/main/java/at/techbee/jtx/ui/compose/screens/AboutLibraries.kt
+++ app/src/main/java/at/techbee/jtx/ui/compose/screens/AboutLibraries.kt
@@ -8,39 +8,47 @@
 
 package at.techbee.jtx.ui.compose.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import at.techbee.jtx.R
 import at.techbee.jtx.ui.theme.JtxBoardTheme
 import at.techbee.jtx.ui.theme.Typography
 import com.mikepenz.aboutlibraries.ui.compose.LibrariesContainer
+import com.mikepenz.aboutlibraries.ui.compose.LibraryDefaults
 
 
 @Composable
 fun AboutLibraries() {
-    Column(
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.padding(start = 8.dp, end = 8.dp, top = 16.dp, bottom = 16.dp)
-    ) {
-        Text(
-            stringResource(id = R.string.about_tabitem_libraries),
-            style = Typography.titleLarge,
+
+    LibrariesContainer(
+        header = {
+            item {
+                Text(
+                    stringResource(id = R.string.about_tabitem_libraries),
+                    style = Typography.titleLarge,
+                    modifier = Modifier.padding(start = 8.dp, end = 8.dp, top = 16.dp, bottom = 16.dp).fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
+            }
+        },
+        modifier = Modifier.fillMaxWidth(),
+        colors = LibraryDefaults.libraryColors(
+            backgroundColor = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.onSurface,
+            badgeBackgroundColor = MaterialTheme.colorScheme.primaryContainer,
+            badgeContentColor = MaterialTheme.colorScheme.onPrimaryContainer
         )
-        LibrariesContainer(
-            Modifier.fillMaxSize()
-        )
-    }
+    )
 }
+
 
 
 @Preview(showBackground = true)
