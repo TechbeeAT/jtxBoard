719b80b8eba72aa897e60e58b0e71fa7feee27ac
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2022-08-20 11:03:07 +0200

    Import optimization

--- app/src/main/java/at/techbee/jtx/ui/compose/elements/FilterSection.kt
+++ app/src/main/java/at/techbee/jtx/ui/compose/elements/FilterSection.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import at.techbee.jtx.R
 
 @Composable
