e6c265fb2ddab6f8021989160497649734ae2f07
Patrick Lang <patrick@techbee.at>
2022-09-02 16:46:07 +0200

    Improved status, classification, priority handling

--- app/src/main/java/at/techbee/jtx/ui/compose/screens/DetailScreenContent.kt
+++ app/src/main/java/at/techbee/jtx/ui/compose/screens/DetailScreenContent.kt
@@ -22,7 +22,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -76,9 +75,6 @@ fun DetailScreenContent(
     val location = rememberSaveable { mutableStateOf(iCalEntity.value?.property?.location ?: "") }
     val geoLat = rememberSaveable { mutableStateOf(iCalEntity.value?.property?.geoLat) }
     val geoLong = rememberSaveable { mutableStateOf(iCalEntity.value?.property?.geoLong) }
-    var status by rememberSaveable { mutableStateOf(iCalEntity.value?.property?.status) }
-    var classification by rememberSaveable { mutableStateOf(iCalEntity.value?.property?.classification) }
-    var priority by rememberSaveable { mutableStateOf(iCalEntity.value?.property?.priority ?: 0) }
     var rrule by rememberSaveable { mutableStateOf(iCalEntity.value?.property?.rrule) }
 
     val icalObject by rememberSaveable { mutableStateOf(iCalEntity.value?.property ?: ICalObject()) }
@@ -293,131 +289,14 @@ fun DetailScreenContent(
             }
 
 
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                var statusMenuExpanded by remember { mutableStateOf(false) }
-                var classificationMenuExpanded by remember { mutableStateOf(false) }
-                var priorityMenuExpanded by remember { mutableStateOf(false) }
-
-                AssistChip(
-                    label = {
-                        if (iCalEntity.value?.property?.component == Component.VJOURNAL.name)
-                            Text(StatusJournal.getStringResource(context, status) ?: status ?: "-")
-                        else
-                            Text(StatusTodo.getStringResource(context, status) ?: status ?: "-")
-
-                        DropdownMenu(
-                            expanded = statusMenuExpanded,
-                            onDismissRequest = { statusMenuExpanded = false }
-                        ) {
-                            if (iCalEntity.value?.property?.component == Component.VJOURNAL.name) {
-                                StatusJournal.values().forEach { statusJournal ->
-                                    DropdownMenuItem(
-                                        text = { Text(stringResource(id = statusJournal.stringResource)) },
-                                        onClick = {
-                                            status = statusJournal.name
-                                            statusMenuExpanded = false
-                                            icalObject.status = status
-                                        }
-                                    )
-                                }
-                            } else {
-                                StatusTodo.values().forEach { statusJournal ->
-                                    DropdownMenuItem(
-                                        text = { Text(stringResource(id = statusJournal.stringResource)) },
-                                        onClick = {
-                                            status = statusJournal.name
-                                            statusMenuExpanded = false
-                                            icalObject.status = status
-                                        }
-                                    )
-                                }
-                            }
-                        }
-                    },
-                    leadingIcon = {
-                        Icon(
-                            Icons.Outlined.PublishedWithChanges,
-                            stringResource(id = R.string.status)
-                        )
-                    },
-                    onClick = { statusMenuExpanded = true }
-                )
-
-
-                AssistChip(
-                    label = {
-                        Text(
-                            Classification.getStringResource(context, classification)
-                                ?: classification ?: ""
-                        )
-
-                        DropdownMenu(
-                            expanded = classificationMenuExpanded,
-                            onDismissRequest = { classificationMenuExpanded = false }
-                        ) {
-                            Classification.values().forEach { clazzification ->
-                                DropdownMenuItem(
-                                    text = { Text(stringResource(id = clazzification.stringResource)) },
-                                    onClick = {
-                                        classification = clazzification.name
-                                        classificationMenuExpanded = false
-                                        icalObject.classification = classification
-                                    }
-                                )
-                            }
-                        }
-                    },
-                    leadingIcon = {
-                        Icon(
-                            Icons.Outlined.GppMaybe,
-                            stringResource(id = R.string.classification)
-                        )
-                    },
-                    onClick = { classificationMenuExpanded = true }
-                )
-
-                val priorityStrings = stringArrayResource(id = R.array.priority)
-                if (iCalEntity.value?.property?.component == Component.VTODO.name) {
-
-                    AssistChip(
-                        label = {
-                            Text(
-                                if (priority in priorityStrings.indices)
-                                    stringArrayResource(id = R.array.priority)[priority]
-                                else
-                                    stringArrayResource(id = R.array.priority)[0]
-                            )
-
-                            DropdownMenu(
-                                expanded = priorityMenuExpanded,
-                                onDismissRequest = { priorityMenuExpanded = false }
-                            ) {
-                                stringArrayResource(id = R.array.priority).forEachIndexed { index, prio ->
-                                    DropdownMenuItem(
-                                        text = { Text(prio) },
-                                        onClick = {
-                                            priority = index
-                                            priorityMenuExpanded = false
-                                            icalObject.priority = priority
-                                        }
-                                    )
-                                }
-                            }
-                        },
-                        leadingIcon = {
-                            Icon(
-                                Icons.Outlined.AssignmentLate,
-                                stringResource(id = R.string.priority)
-                            )
-                        },
-                        onClick = { priorityMenuExpanded = true }
-                    )
-                }
-            }
+            DetailsCardStatusClassificationPriority(
+                icalObject = icalObject,
+                isEditMode = isEditMode.value,
+                onStatusChanged = { newStatus -> icalObject.status = newStatus },
+                onClassificationChanged = { newClassification -> icalObject.classification = newClassification },
+                onPriorityChanged = { newPriority -> icalObject.priority = newPriority },
+                modifier = Modifier.fillMaxWidth()
+            )
 
             AnimatedVisibility(subtasks.value.isNotEmpty() || isEditMode.value) {
                 DetailsCardSubtasks(
