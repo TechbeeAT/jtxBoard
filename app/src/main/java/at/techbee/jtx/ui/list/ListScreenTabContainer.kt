e8b874c28bfd3b6c74fce4d7c8622c3caefd9cec
Patrick Lang <patrick.lang85@gmail.com>
2022-10-12 20:52:19 +0200

    Resolve Quick add crashes by changing quick add again to dialog Closes #421

--- app/src/main/java/at/techbee/jtx/ui/list/ListScreenTabContainer.kt
+++ app/src/main/java/at/techbee/jtx/ui/list/ListScreenTabContainer.kt
@@ -23,26 +23,9 @@ import androidx.compose.material.icons.outlined.DeleteOutline
 import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material.icons.outlined.Sync
 import androidx.compose.material.rememberModalBottomSheetState
-import androidx.compose.material3.Divider
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
-import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDrawerState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -375,8 +358,7 @@ fun ListScreenTabContainer(
                                             null  // origin was state from import
                                         globalStateHolder.icalFromIntentAttachment.value =
                                             null  // origin was state from import
-                                    },
-                                    modifier = Modifier.shadow(1.dp).padding(8.dp)
+                                    }
                                 )
                             }
 
