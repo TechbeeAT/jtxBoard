1a4602a251df8afa88c39f03652d50595eafb5ee
Patrick Lang <patrick.lang85@gmail.com>
2022-10-27 00:17:58 +0200

    Added Worker to periodically update widget

--- app/src/main/java/at/techbee/jtx/widgets/JournalsWidgetReceiver.kt
+++ app/src/main/java/at/techbee/jtx/widgets/JournalsWidgetReceiver.kt
@@ -9,6 +9,7 @@
 package at.techbee.jtx.widgets
 
 import android.appwidget.AppWidgetManager
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.util.Log
@@ -18,6 +19,7 @@ import androidx.glance.appwidget.GlanceAppWidgetManager
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.state.updateAppWidgetState
 import androidx.glance.state.PreferencesGlanceStateDefinition
+import androidx.work.*
 import at.techbee.jtx.database.ICalDatabase
 import at.techbee.jtx.database.Module
 import kotlinx.coroutines.Dispatchers
@@ -25,6 +27,7 @@ import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import java.util.concurrent.TimeUnit
 
 private const val TAG = "JournalsWidgetRec"
 
@@ -41,11 +44,15 @@ class JournalsWidgetReceiver : GlanceAppWidgetReceiver() {
     ) {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
         observeData(context)
+        setWork(context)
     }
 
     override fun onReceive(context: Context, intent: Intent) {
         super.onReceive(context, intent)
-        observeData(context)
+        //if(intent.action == AppWidgetManager.ACTION_APPWIDGET_UPDATE) {
+            observeData(context)
+            setWork(context)
+        //}
     }
 
     private fun observeData(context: Context) {
@@ -65,12 +72,33 @@ class JournalsWidgetReceiver : GlanceAppWidgetReceiver() {
                         }
                     }
                     glanceAppWidget.update(context, it)
+                    Log.d(TAG, "Widget updated")
                 }
             }
         }
     }
 
+    private fun setWork(context: Context) {
+
+        val work: PeriodicWorkRequest = PeriodicWorkRequestBuilder<JournalsWidgetUpdateWorker>(5, TimeUnit.MINUTES).build()
+        WorkManager
+            .getInstance(context)
+            .enqueueUniquePeriodicWork("journalWidgetWorker", ExistingPeriodicWorkPolicy.KEEP, work)
+        Log.d(TAG, "Work enqueued")
+    }
+
     companion object {
         val journalsList = stringSetPreferencesKey("journalsList")
+
+        fun updateJournalsWidgets(context: Context) {
+            val widgetProvider = JournalsWidgetReceiver::class.java
+            val comp = ComponentName(context, widgetProvider)
+            val ids = AppWidgetManager.getInstance(context).getAppWidgetIds(comp)
+            val intent = Intent(context, widgetProvider).apply {
+                this.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
+                this.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids)
+            }
+            context.sendBroadcast(intent)
+        }
     }
 }
---
+++ app/src/main/java/at/techbee/jtx/widgets/JournalsWidgetUpdateWorker.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) Techbee e.U.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ */
+
+package at.techbee.jtx.widgets
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+
+
+class JournalsWidgetUpdateWorker(
+    private val context: Context,
+    workerParameters: WorkerParameters
+) : CoroutineWorker(
+    context,
+    workerParameters
+) {
+    override suspend fun doWork(): Result {
+        JournalsWidgetReceiver.updateJournalsWidgets(context)
+        return Result.success()
+    }
+}
