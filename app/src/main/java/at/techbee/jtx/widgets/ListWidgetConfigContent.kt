bb27d4fc04f8b5ec47a26da364f379f4c7c25034
Patrick Lang <patrick.lang85@gmail.com>
2022-11-08 21:12:37 +0100

    Fixed merge issue

--- app/src/main/java/at/techbee/jtx/widgets/ListWidgetConfigContent.kt
+++ app/src/main/java/at/techbee/jtx/widgets/ListWidgetConfigContent.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import at.techbee.jtx.ListSettings
 import at.techbee.jtx.R
 import at.techbee.jtx.database.*
 import at.techbee.jtx.ui.list.*
