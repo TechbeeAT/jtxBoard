524150bbd88b8d4f439f83e6d5fb27f0c23391f1
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2022-11-24 13:56:55 +0100

    consolidated code, removed onReceive method

--- app/src/main/java/at/techbee/jtx/widgets/ListWidgetReceiver.kt
+++ app/src/main/java/at/techbee/jtx/widgets/ListWidgetReceiver.kt
@@ -10,7 +10,6 @@ package at.techbee.jtx.widgets
 
 import android.appwidget.AppWidgetManager
 import android.content.Context
-import android.content.Intent
 import android.os.Build
 import android.util.Log
 import androidx.datastore.preferences.core.stringPreferencesKey
@@ -35,17 +34,7 @@ class ListWidgetReceiver : GlanceAppWidgetReceiver() {
     ) {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
         setOneTimeWork(context)
-        //setPeriodicWork(context)
     }
-
-    override fun onReceive(context: Context, intent: Intent) {
-        super.onReceive(context, intent)
-        //if(intent.action == AppWidgetManager.ACTION_APPWIDGET_UPDATE) {
-            setOneTimeWork(context)
-            //setPeriodicWork(context)
-        //}
-    }
-
 
     companion object {
         val list = stringSetPreferencesKey("list")
@@ -53,37 +42,26 @@ class ListWidgetReceiver : GlanceAppWidgetReceiver() {
         val subnotes = stringSetPreferencesKey("subnotes")
         val filterConfig = stringPreferencesKey("filter_config")
 
-        /*
-        private fun setPeriodicWork(context: Context) {
 
-            val work: PeriodicWorkRequest = PeriodicWorkRequestBuilder<ListWidgetUpdateWorker>(5, TimeUnit.MINUTES).build()
-            WorkManager
-                .getInstance(context)
-                .enqueueUniquePeriodicWork("listWidgetWorker", ExistingPeriodicWorkPolicy.KEEP, work)
-            Log.d(TAG, "Work enqueued")
-        }
+        /**
+         * Sets a worker to update the widget
+         * @param delay: if true the update of the widget is delayed by 2 seconds and the existing work is replaced ( in order to not trigger multiple works in a row )
+         * if false the update of the widget is done immediately
          */
-
-        fun setOneTimeWork(context: Context) {
-            val work: OneTimeWorkRequest = OneTimeWorkRequestBuilder<ListWidgetUpdateWorker>().build()
+        fun setOneTimeWork(context: Context, delay: Boolean? = false) {
+            val work: OneTimeWorkRequest = OneTimeWorkRequestBuilder<ListWidgetUpdateWorker>()
+                .apply {
+                    if (delay == true && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                        setInitialDelay((2).seconds.toJavaDuration())
+                }.build()
             WorkManager
                 .getInstance(context)
-                .enqueueUniqueWork("listWidgetOneTimeWorker", ExistingWorkPolicy.KEEP, work)
+                .enqueueUniqueWork(
+                    "listWidgetOneTimeWorker",
+                    if (delay == true && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) ExistingWorkPolicy.REPLACE else ExistingWorkPolicy.KEEP,
+                    work
+                )
             Log.d(TAG, "Work enqueued")
-        }
-
-        fun setDelayedOneTimeWork(context: Context) {
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-                setOneTimeWork(context)
-            } else {
-                val work: OneTimeWorkRequest = OneTimeWorkRequestBuilder<ListWidgetUpdateWorker>()
-                    .setInitialDelay((2).seconds.toJavaDuration())
-                    .build()
-                WorkManager
-                    .getInstance(context)
-                    .enqueueUniqueWork("listWidgetOneTimeWorker", ExistingWorkPolicy.REPLACE, work)
-                Log.d(TAG, "Work enqueued")
-            }
         }
     }
 }
