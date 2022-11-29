e1a895e5e3d862b90fa557c706e7b99f83aae67a
Patrick Lang <patrick.lang85@gmail.com>
2022-11-29 21:43:03 +0000

    Resolve "Make groups expandable/collapsable"

--- app/src/main/java/at/techbee/jtx/ui/list/ListScreenCompact.kt
+++ app/src/main/java/at/techbee/jtx/ui/list/ListScreenCompact.kt
@@ -16,22 +16,25 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowDropDown
+import androidx.compose.material.icons.outlined.ArrowDropUp
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import at.techbee.jtx.R
 import at.techbee.jtx.database.*
 import at.techbee.jtx.database.views.ICal4List
 import at.techbee.jtx.flavored.BillingManager
@@ -53,6 +56,8 @@ fun ListScreenCompact(
     val scrollId by scrollOnceId.observeAsState(null)
     val listState = rememberLazyListState()
 
+    val itemsCollapsed = remember { mutableStateListOf<String>() }
+
 
     LazyColumn(
         modifier = Modifier.padding(start = 2.dp, end = 2.dp),
@@ -61,72 +66,90 @@ fun ListScreenCompact(
 
         groupedList.forEach { (groupName, group) ->
 
-            if(groupedList.keys.size > 1) {
+            if (groupedList.keys.size > 1) {
                 stickyHeader {
-                    Text(
-                        text = groupName,
-                        modifier = Modifier.fillMaxWidth()
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
                             .background(MaterialTheme.colorScheme.background)
-                            .padding(top = 8.dp, bottom = 8.dp),
-                        style = MaterialTheme.typography.titleLarge,
-                        textAlign = TextAlign.Center
-                    )
-                }
-            }
 
+                    ) {
+                        Text(
+                            text = groupName,
+                            style = MaterialTheme.typography.titleLarge
+                        )
 
-            items(
-            items = group,
-            key = { item -> item.id }
-        )
-        { iCalObject ->
-
-            var currentSubtasks = subtasks[iCalObject.uid]
-            if(listSettings.isExcludeDone.value)   // exclude done if applicable
-
-                currentSubtasks = currentSubtasks?.filter { subtask -> subtask.percent != 100 }
-
-
-            if(scrollId != null) {
-                LaunchedEffect(group) {
-                    val index = group.indexOfFirst { iCalObject -> iCalObject.id == scrollId }
-                    if(index > -1) {
-                        listState.animateScrollToItem(index)
-                        scrollOnceId.postValue(null)
+                        TextButton(onClick = {
+                            if (itemsCollapsed.contains(groupName))
+                                itemsCollapsed.remove(groupName)
+                            else
+                                itemsCollapsed.add(groupName)
+                        }) {
+                            if (itemsCollapsed.contains(groupName))
+                                Icon(Icons.Outlined.ArrowDropUp, stringResource(R.string.list_collapse))
+                            else
+                                Icon(Icons.Outlined.ArrowDropDown, stringResource(R.string.list_expand))
+                        }
                     }
                 }
             }
 
-            ListCardCompact(
-                iCalObject,
-                subtasks = currentSubtasks ?: emptyList(),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 4.dp, bottom = 4.dp)
-                    .animateItemPlacement()
-                    .clip(jtxCardCornerShape)
-                    .combinedClickable(
-                        onClick = { goToDetail(iCalObject.id, false, groupedList.flatMap { it.value }) },
-                        onLongClick = {
-                            if (!iCalObject.isReadOnly && BillingManager.getInstance().isProPurchased.value == true)
-                                goToDetail(iCalObject.id, false, groupedList.flatMap { it.value })
-                        }
-                    ),
-                onProgressChanged = onProgressChanged,
-                goToDetail = goToDetail
-            )
-
-            if (iCalObject != group.last())
-                Divider(
-                    color = MaterialTheme.colorScheme.onSurfaceVariant,
-                    thickness = 1.dp,
-                    modifier = Modifier.alpha(0.25f)
+            if (groupedList.keys.size <= 1 || (groupedList.keys.size > 1 && !itemsCollapsed.contains(groupName))) {
+                items(
+                    items = group,
+                    key = { item -> item.id }
                 )
-        }
+                { iCalObject ->
+
+                    var currentSubtasks = subtasks[iCalObject.uid]
+                    if (listSettings.isExcludeDone.value)   // exclude done if applicable
+
+                        currentSubtasks = currentSubtasks?.filter { subtask -> subtask.percent != 100 }
+
+
+                    if (scrollId != null) {
+                        LaunchedEffect(group) {
+                            val index = group.indexOfFirst { iCalObject -> iCalObject.id == scrollId }
+                            if (index > -1) {
+                                listState.animateScrollToItem(index)
+                                scrollOnceId.postValue(null)
+                            }
+                        }
+                    }
+
+                    ListCardCompact(
+                        iCalObject,
+                        subtasks = currentSubtasks ?: emptyList(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 4.dp, bottom = 4.dp)
+                            .animateItemPlacement()
+                            .clip(jtxCardCornerShape)
+                            .combinedClickable(
+                                onClick = { goToDetail(iCalObject.id, false, groupedList.flatMap { it.value }) },
+                                onLongClick = {
+                                    if (!iCalObject.isReadOnly && BillingManager.getInstance().isProPurchased.value == true)
+                                        goToDetail(iCalObject.id, false, groupedList.flatMap { it.value })
+                                }
+                            ),
+                        onProgressChanged = onProgressChanged,
+                        goToDetail = goToDetail
+                    )
+
+                    if (iCalObject != group.last())
+                        Divider(
+                            color = MaterialTheme.colorScheme.onSurfaceVariant,
+                            thickness = 1.dp,
+                            modifier = Modifier.alpha(0.25f)
+                        )
+                }
+            }
         }
     }
 }
-
 
 
 @Preview(showBackground = true)
@@ -151,7 +174,8 @@ fun ListScreenCompact_TODO() {
             numAttachments = 0
             numSubnotes = 0
             numSubtasks = 0
-            summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            summary =
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         }
         val icalobject2 = ICal4List.getSample().apply {
             id = 2L
@@ -162,7 +186,8 @@ fun ListScreenCompact_TODO() {
             classification = Classification.CONFIDENTIAL.name
             dtstart = System.currentTimeMillis()
             due = System.currentTimeMillis()
-            summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            summary =
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             colorItem = Color.Blue.toArgb()
         }
         ListScreenCompact(
@@ -175,7 +200,6 @@ fun ListScreenCompact_TODO() {
         )
     }
 }
-
 
 
 @Preview(showBackground = true)
@@ -201,7 +225,8 @@ fun ListScreenCompact_JOURNAL() {
             numAttachments = 0
             numSubnotes = 0
             numSubtasks = 0
-            summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            summary =
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         }
         val icalobject2 = ICal4List.getSample().apply {
             id = 2L
@@ -212,7 +237,8 @@ fun ListScreenCompact_JOURNAL() {
             classification = Classification.CONFIDENTIAL.name
             dtstart = System.currentTimeMillis()
             due = System.currentTimeMillis()
-            summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            summary =
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             colorItem = Color.Blue.toArgb()
         }
         ListScreenCompact(
