8354860f701351d86bd49fd7598d221b2a6fdd8a
Patrick Lang <patrick.lang85@gmail.com>
2022-12-08 12:12:26 +0100

    [Feature] Show link to GitHub releases additionally for release notes Closes #20

--- app/src/main/java/at/techbee/jtx/ui/about/AboutReleaseinfo.kt
+++ app/src/main/java/at/techbee/jtx/ui/about/AboutReleaseinfo.kt
@@ -10,18 +10,24 @@ package at.techbee.jtx.ui.about
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.OpenInNew
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.MutableLiveData
@@ -54,15 +60,35 @@ fun AboutReleaseinfo(
             )
         }
 
-
         items(
             items = list.toList(),
             key = { release -> release.releaseName }
         ) { release ->
             ReleaseInfoCard(
                 release = release,
-                modifier = Modifier.padding(top = 8.dp).animateItemPlacement()
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .animateItemPlacement()
             )
+        }
+
+
+        item {
+            TextButton(onClick = { /*TODO*/ }) {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally),
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(vertical = 16.dp, horizontal = 32.dp)
+                ) {
+                    Text(
+                        text = "https://github.com/TechbeeAT/jtxBoard/releases",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier.weight(1f)
+                    )
+                    Icon(Icons.Outlined.OpenInNew, contentDescription = null)
+                }
+            }
         }
     }
 }
