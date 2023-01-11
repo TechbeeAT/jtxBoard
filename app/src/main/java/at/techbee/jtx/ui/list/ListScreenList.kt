53887330a9ddb491cb4e475a17a1f374bdcb0049
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2023-01-11 12:41:04 +0100

    baseline profiles (#142)
    * basic implementation of benchmark and baseline profile done, needs some tweaks/improvements now
    
    * improved baseline profile and benchmark with additional action
     added proguard-rules for benchmark

--- app/src/main/java/at/techbee/jtx/ui/list/ListScreenList.kt
+++ app/src/main/java/at/techbee/jtx/ui/list/ListScreenList.kt
@@ -32,6 +32,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -173,6 +174,7 @@ fun ListScreenList(
                                         onLongClick(iCalObject.id, groupedList.flatMap { it.value })
                                 }
                             )
+                            .testTag("benchmark:ListCard")
                     )
                 }
             }
