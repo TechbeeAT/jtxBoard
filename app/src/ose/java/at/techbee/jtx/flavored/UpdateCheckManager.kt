534746e80df013f7df1417d0e1d467129ab69fa5
Patrick Lang <patrick.lang85@gmail.com>
2023-04-16 09:58:21 +0200

    Added UpdateCheckManager stubs for other flavors

---
+++ app/src/ose/java/at/techbee/jtx/flavored/UpdateCheckManager.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Techbee e.U.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ */
+
+package at.techbee.jtx.flavored
+
+import android.app.Activity
+
+
+
+class UpdateCheckManager(val activity: Activity) : UpdateCheckManagerDefinition {
+
+    override fun checkForUpdates() {
+        // currently only used in HUAWEI
+    }
+}
