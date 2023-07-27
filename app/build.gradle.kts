5e0d42189792078eb4f5e7d0d494b61281c3dc96
Patrick Lang <patrick.lang85@gmail.com>
2023-07-27 13:32:27 +0200

    wip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -212,7 +212,7 @@ tasks.register('updateLocalesConfig') {
 }
 
 gradle.projectsEvaluated {
-    preBuild.dependsOn('updateLocalesConfig')
+    preBuild.dependsOn("updateLocalesConfig")
 }
 
 configurations {
@@ -229,116 +229,116 @@ configurations {
 
 dependencies {
 
-    implementation "com.github.bitfireAT:ical4android:a78e72f580"
+    implementation("com.github.bitfireAT:ical4android:a78e72f580")
 
-    implementation fileTree(dir: 'libs', include: ['*.jar'])
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
     // Android KTX
-    implementation "androidx.core:core-ktx:$version_core"
-    implementation "org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1"
+    implementation("androidx.core:core-ktx:$version_core")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 
     // Android Annotations
-    implementation "androidx.annotation:annotation:1.6.0"
+    implementation("androidx.annotation:annotation:1.6.0")
 
     // Room dependencies
-    implementation "androidx.room:room-runtime:$version_room"
-    implementation "androidx.room:room-ktx:$version_room"   // Kotlin Extensions and Coroutines support for Room
-    kapt "androidx.room:room-compiler:$version_room"
+    implementation("androidx.room:room-runtime:$version_room")
+    implementation("androidx.room:room-ktx:$version_room")   // Kotlin Extensions and Coroutines support for Room
+    kapt("androidx.room:room-compiler:$version_room")
 
     // Coroutines
-    implementation "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version_coroutine"
-    implementation "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version_coroutine"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$version_coroutine")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$version_coroutine")
 
-    implementation "androidx.preference:preference-ktx:1.2.0"
+    implementation("androidx.preference:preference-ktx:1.2.0")
 
     // Volley for simple network requests
-    implementation "com.android.volley:volley:1.2.1"
+    implementation("com.android.volley:volley:1.2.1")
 
     // Google billing
-    gplayImplementation "com.android.billingclient:billing-ktx:$version_billing"
+    gplayImplementation("com.android.billingclient:billing-ktx:$version_billing")
     // Google request for review
-    gplayImplementation "com.google.android.play:review:$version_review"
-    gplayImplementation "com.google.android.play:review-ktx:$version_review"
+    gplayImplementation("com.google.android.play:review:$version_review")
+    gplayImplementation("com.google.android.play:review-ktx:$version_review")
 
     // Amazon billing & maps support
-    amazonImplementation 'com.amazon.device:amazon-appstore-sdk:3.0.4'
-    amazonImplementation "com.google.maps.android:maps-compose:2.12.0"
-    amazonImplementation "com.google.android.gms:play-services-maps:18.1.0"
-    amazonImplementation "com.google.android.gms:play-services-location:21.0.1"
+    amazonImplementation("com.amazon.device:amazon-appstore-sdk:3.0.4")
+    amazonImplementation("com.google.maps.android:maps-compose:2.12.0")
+    amazonImplementation("com.google.android.gms:play-services-maps:18.1.0")
+    amazonImplementation("com.google.android.gms:play-services-location:21.0.1")
 
     // Huawei billing
-    //huaweiImplementation "com.huawei.hms:iap:$version_huawei_iap"
+    //huaweiImplementation("com.huawei.hms:iap:$version_huawei_iap"
 
     // Google Maps
-    gplayImplementation "com.google.maps.android:maps-compose:2.12.0"
-    gplayImplementation "com.google.android.gms:play-services-maps:18.1.0"
-    gplayImplementation "com.google.android.gms:play-services-location:21.0.1"
+    gplayImplementation("com.google.maps.android:maps-compose:2.12.0")
+    gplayImplementation("com.google.android.gms:play-services-maps:18.1.0")
+    gplayImplementation("com.google.android.gms:play-services-location:21.0.1")
 
     //OSM
-    oseImplementation "org.osmdroid:osmdroid-android:$version_osmdroid"
-    genericImplementation "org.osmdroid:osmdroid-android:$version_osmdroid"
-    huaweiImplementation "org.osmdroid:osmdroid-android:$version_osmdroid"
+    oseImplementation("org.osmdroid:osmdroid-android:$version_osmdroid")
+    genericImplementation("org.osmdroid:osmdroid-android:$version_osmdroid")
+    huaweiImplementation("org.osmdroid:osmdroid-android:$version_osmdroid")
 
     // Workmanager support
-    implementation "androidx.work:work-runtime-ktx:$version_work"
-    androidTestImplementation "androidx.work:work-testing:$version_work"
+    implementation("androidx.work:work-runtime-ktx:$version_work")
+    androidTestImplementation("androidx.work:work-testing:$version_work")
 
     //support for java.time for older Android versions (<O)
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 
-    androidTestImplementation "androidx.test.ext:junit-ktx:1.1.5"
-    //androidTestImplementation "androidx.test.espresso:espresso-core:3.5.0-alpha07"
-    androidTestImplementation "androidx.arch.core:core-testing:2.2.0"
-    androidTestImplementation "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version_coroutine"
-    androidTestImplementation "androidx.test:runner:1.5.2"
-    androidTestImplementation "androidx.test:rules:1.5.0"
+    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.5")
+    //androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0-alpha07")
+    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
+    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$version_coroutine")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
 
     // Testing-only dependencies
-    androidTestImplementation "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version_kotlin"
-    androidTestImplementation "androidx.test:core-ktx:1.5.0"
+    androidTestImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version_kotlin")
+    androidTestImplementation("androidx.test:core-ktx:1.5.0")
 
     // Testing
-    testImplementation "junit:junit:4.13.2"
-    testImplementation "androidx.room:room-testing:$version_room"
-    testImplementation "androidx.arch.core:core-testing:2.2.0"
-    testImplementation "androidx.test:core-ktx:1.5.0"
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.room:room-testing:$version_room")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("androidx.test:core-ktx:1.5.0")
     // Required -- JUnit 4 framework
-    testImplementation "androidx.test.ext:junit-ktx:1.1.5"
-    //testImplementation "org.mockito:mockito-core:5.3.1"
+    testImplementation("androidx.test.ext:junit-ktx:1.1.5")
+    //testImplementation("org.mockito:mockito-core:5.3.1")
 
     //About Libraries
-    implementation "com.mikepenz:aboutlibraries-core:${version_about_libraries}"
-    implementation "com.mikepenz:aboutlibraries-compose:${version_about_libraries}"
+    implementation("com.mikepenz:aboutlibraries-core:${version_about_libraries}")
+    implementation("com.mikepenz:aboutlibraries-compose:${version_about_libraries}")
 
     // Compose Color Picker
-    implementation "com.godaddy.android.colorpicker:compose-color-picker-android:0.7.0"
+    implementation("com.godaddy.android.colorpicker:compose-color-picker-android:0.7.0")
 
     // appcompat for language selection
-    implementation "androidx.appcompat:appcompat:$version_appcompat"
+    implementation("androidx.appcompat:appcompat:$version_appcompat")
 
     // jetpack compose
-    implementation 'androidx.core:core-ktx:1.10.1'
-    implementation "androidx.compose.ui:ui:$version_compose"
-    implementation "androidx.compose.material3:material3:1.2.0-alpha04"
-    implementation "androidx.compose.material:material-icons-extended:1.4.3"
-    implementation "androidx.compose.material:material:1.4.3"
-    implementation "androidx.compose.ui:ui-tooling-preview:$version_compose"
-    //implementation "androidx.activity:activity-compose:1.6.0"
-    implementation "androidx.compose.runtime:runtime-livedata:$version_compose"
-    implementation "androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1"
-    implementation "androidx.navigation:navigation-compose:2.7.0-rc01"
-    implementation "com.arnyminerz.markdowntext:markdowntext:1.3.2"
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.compose.ui:ui:$version_compose")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha04")
+    implementation("androidx.compose.material:material-icons-extended:1.4.3")
+    implementation("androidx.compose.material:material:1.4.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:$version_compose")
+    //implementation("androidx.activity:activity-compose:1.6.0")
+    implementation("androidx.compose.runtime:runtime-livedata:$version_compose")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.navigation:navigation-compose:2.7.0-rc01")
+    implementation("com.arnyminerz.markdowntext:markdowntext:1.3.2")
 
-    androidTestImplementation "androidx.compose.ui:ui-test-junit4:$version_compose"
-    debugImplementation "androidx.compose.ui:ui-tooling:$version_compose"
-    debugImplementation "androidx.compose.ui:ui-test-manifest:$version_compose"
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$version_compose")
+    debugImplementation("androidx.compose.ui:ui-tooling:$version_compose")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:$version_compose")
 
     // permissions from accompanist
-    implementation "com.google.accompanist:accompanist-permissions:$version_accompanist"
+    implementation("com.google.accompanist:accompanist-permissions:$version_accompanist")
 
-    implementation "androidx.glance:glance-appwidget:1.0.0-alpha05"
+    implementation("androidx.glance:glance-appwidget:1.0.0-alpha05")
 
-    implementation "androidx.biometric:biometric-ktx:1.2.0-alpha05"
+    implementation("androidx.biometric:biometric-ktx:1.2.0-alpha05")
     
-    implementation "androidx.profileinstaller:profileinstaller:1.3.1"
+    implementation("androidx.profileinstaller:profileinstaller:1.3.1")
 }
