3bc29213c15eb993bf712ef70c0dab5b3215a8e4
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2023-11-03 21:30:05 +0100

    replaced enum values() by entries

--- app/src/main/java/at/techbee/jtx/database/Converters.kt
+++ app/src/main/java/at/techbee/jtx/database/Converters.kt
@@ -10,7 +10,6 @@ package at.techbee.jtx.database
 
 import androidx.room.TypeConverter
 import at.techbee.jtx.database.locals.StoredListSettingData
-import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
@@ -19,9 +18,9 @@ class Converters {
     @TypeConverter fun listSettingsParcelToString(value: StoredListSettingData) = Json.encodeToString(value)
     @TypeConverter fun stringToListSettingsParcel(value: String) = Json.decodeFromString<StoredListSettingData>(value)
 
-    @TypeConverter fun stringToStatus(value: String) = Status.values().find { it.name == value } ?: Status.NO_STATUS
+    @TypeConverter fun stringToStatus(value: String) = Status.entries.find { it.name == value } ?: Status.NO_STATUS
     @TypeConverter fun statusToString(value: Status?) = value?.name
 
-    @TypeConverter fun stringToModule(value: String) = Module.values().find { it.name == value } ?: Module.NOTE
+    @TypeConverter fun stringToModule(value: String) = Module.entries.find { it.name == value } ?: Module.NOTE
     @TypeConverter fun moduleToString(value: Module?) = value?.name
 }
