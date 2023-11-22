1f280b8dc8bd51af5839786af4d70280847fed8e
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2023-11-22 21:48:18 +0100

    Fix for [BUG] Text date field disappears during editing #1035

--- app/src/main/java/at/techbee/jtx/ui/reusable/dialogs/DatePickerDialog.kt
+++ app/src/main/java/at/techbee/jtx/ui/reusable/dialogs/DatePickerDialog.kt
@@ -9,16 +9,41 @@
 package at.techbee.jtx.ui.reusable.dialogs
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.MoreTime
 import androidx.compose.material.icons.outlined.Today
 import androidx.compose.material.icons.outlined.TravelExplore
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SecondaryTabRow
+import androidx.compose.material3.SelectableDates
+import androidx.compose.material3.Tab
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalInspectionMode
@@ -35,7 +60,7 @@ import at.techbee.jtx.util.DateTimeUtils
 import java.time.Instant
 import java.time.ZoneId
 import java.time.ZonedDateTime
-import java.util.*
+import java.util.TimeZone
 import kotlin.collections.contains
 
 
@@ -113,6 +138,7 @@ fun DatePickerDialog(
                                             checked = datePickerState.selectedDateMillis != null,
                                             onCheckedChange = {
                                                 datePickerState.selectedDateMillis = if (it) defaultDateTime.withZoneSameLocal(ZoneId.of("UTC")).toInstant().toEpochMilli() else null
+                                                datePickerState.displayMode = DisplayMode.Picker
                                                 newTimezone = if (it) TZ_ALLDAY else null
                                                 selectedTab = tabIndexDate
                                             },
@@ -183,7 +209,7 @@ fun DatePickerDialog(
                     }
                 }
 
-                AnimatedVisibility(selectedTab == tabIndexDate && datePickerState.selectedDateMillis == null) {
+                AnimatedVisibility(selectedTab == tabIndexDate && datePickerState.selectedDateMillis == null && datePickerState.displayMode != DisplayMode.Input) {
                     Text(
                         stringResource(id = R.string.not_set2),
                         fontStyle = FontStyle.Italic,
@@ -194,7 +220,7 @@ fun DatePickerDialog(
                     )
                 }
 
-                AnimatedVisibility(selectedTab == tabIndexDate && datePickerState.selectedDateMillis != null) {
+                AnimatedVisibility(selectedTab == tabIndexDate && (datePickerState.selectedDateMillis != null || datePickerState.displayMode == DisplayMode.Input)) {
                     Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
                         DatePicker(
                             state = datePickerState,
