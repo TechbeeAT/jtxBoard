9f5ebadaa0a3d9d4dde5098c8f4cb737ec20a256
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2024-01-02 21:42:46 +0100

    updated contact field to parse email and telephone number to initiate an email or a call (#1118)

--- app/src/main/java/at/techbee/jtx/ui/detail/DetailsCardContact.kt
+++ app/src/main/java/at/techbee/jtx/ui/detail/DetailsCardContact.kt
@@ -9,22 +9,41 @@
 package at.techbee.jtx.ui.detail
 
 import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.relocation.BringIntoViewRequester
 import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Call
 import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material.icons.outlined.ContactMail
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.outlined.Mail
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onPlaced
@@ -36,6 +55,7 @@ import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.util.PatternsCompat
 import at.techbee.jtx.R
 import at.techbee.jtx.database.properties.Attendee
 import at.techbee.jtx.ui.reusable.dialogs.RequestPermissionDialog
@@ -45,10 +65,12 @@ import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.permissions.shouldShowRationale
+import com.google.i18n.phonenumbers.PhoneNumberUtil
 import kotlinx.coroutines.launch
+import java.util.Locale
 
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class,
+@OptIn(ExperimentalFoundationApi::class,
     ExperimentalPermissionsApi::class
 )
 @Composable
@@ -70,22 +92,76 @@ fun DetailsCardContact(
     val coroutineScope = rememberCoroutineScope()
     var searchContacts = emptyList<Attendee>()
 
+    val foundTelephoneNumber = contact.let {
+        PhoneNumberUtil.getInstance().findNumbers(
+            it, Locale.getDefault().country
+        ).firstOrNull()?.number()?.rawInput
+    }
+
+    val foundEmail = contact.let {
+        val matcher = PatternsCompat.EMAIL_ADDRESS.matcher(it)
+        if(matcher.find())
+            matcher.group()
+        else
+            null
+    }
+
 
     ElevatedCard(modifier = modifier) {
     Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),) {
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+    ) {
 
-            Crossfade(isEditMode) {
+            Crossfade(isEditMode, label = "isEditModeContact") {
                 if(!it) {
 
-                    Column {
-                        HeadlineWithIcon(icon = Icons.Outlined.ContactMail, iconDesc = headline, text = headline)
-                        Text(
-                            text = contact,
-                            modifier = Modifier.fillMaxWidth()
-                        )
+                    Row {
+                        Column(modifier = Modifier.weight(1f)) {
+                            HeadlineWithIcon(
+                                icon = Icons.Outlined.ContactMail,
+                                iconDesc = headline,
+                                text = headline
+                            )
+                            Text(
+                                text = contact,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+
+                        foundTelephoneNumber?.let {
+                            IconButton(onClick = {
+                                val intent = Intent(Intent.ACTION_DIAL).apply {
+                                    data = Uri.parse("tel:$it")
+                                }
+                                if (intent.resolveActivity(context.packageManager) != null) {
+                                    context.startActivity(intent)
+                                }
+                            }) {
+                                Icon(Icons.Outlined.Call, stringResource(
+                                    id = R.string.call_contact,
+                                    it
+                                ))
+                            }
+                        }
+
+                        foundEmail?.let {
+                            IconButton(onClick = {
+                                val intent = Intent(Intent.ACTION_SEND).apply {
+                                    type = "*/*"
+                                    putExtra(Intent.EXTRA_EMAIL, it)
+                                }
+                                if (intent.resolveActivity(context.packageManager) != null) {
+                                    context.startActivity(intent)
+                                }
+                            }) {
+                                Icon(Icons.Outlined.Mail, stringResource(
+                                    id = R.string.email_contact,
+                                    it
+                                ))
+                            }
+                        }
                     }
                 } else {
 
@@ -176,6 +252,19 @@ fun DetailsCardContact_Preview() {
     MaterialTheme {
         DetailsCardContact(
             initialContact = "John Doe, +1 555 5545",
+            isEditMode = false,
+            onContactUpdated = {  }
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DetailsCardContact_Preview_tel() {
+    MaterialTheme {
+        DetailsCardContact(
+            initialContact = "John Doe, +43 676 12 34 567, john@doe.com",
             isEditMode = false,
             onContactUpdated = {  }
         )
