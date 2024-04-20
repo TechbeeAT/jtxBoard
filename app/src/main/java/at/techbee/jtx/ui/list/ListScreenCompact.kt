dd84ebfa755e7311ebf36d2b3174467038c87eac
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2024-04-20 22:36:46 +0200

    Replace pullRefresh from Material2 with pullToRefresh from Material3 (#1352)

--- app/src/main/java/at/techbee/jtx/ui/list/ListScreenCompact.kt
+++ app/src/main/java/at/techbee/jtx/ui/list/ListScreenCompact.kt
@@ -19,25 +19,25 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowDropDown
 import androidx.compose.material.icons.outlined.ArrowDropUp
 import androidx.compose.material.icons.outlined.VerticalAlignTop
-import androidx.compose.material.pullrefresh.PullRefreshIndicator
-import androidx.compose.material.pullrefresh.pullRefresh
-import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -52,6 +52,7 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -75,7 +76,7 @@ import sh.calvin.reorderable.rememberReorderableLazyColumnState
 import java.util.UUID
 
 
-@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun ListScreenCompact(
     groupedList: Map<String, List<ICal4ListRel>>,
@@ -111,20 +112,22 @@ fun ListScreenCompact(
     val storedStatuses by storedStatusesLive.observeAsState(emptyList())
     val storedCategories by storedCategoriesLive.observeAsState(emptyList())
 
-    val pullRefreshState = rememberPullRefreshState(
-        refreshing = false,
-        onRefresh = { onSyncRequested() }
+    val pullToRefreshState = rememberPullToRefreshState(
+        enabled = { isPullRefreshEnabled }
     )
+    LaunchedEffect(pullToRefreshState.isRefreshing) {
+        if(pullToRefreshState.isRefreshing) {
+            onSyncRequested()
+            pullToRefreshState.endRefresh()
+        }
+    }
 
     Box(
         contentAlignment = Alignment.TopCenter,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize().nestedScroll(pullToRefreshState.nestedScrollConnection)
     ) {
         LazyColumn(
-            modifier = if(isPullRefreshEnabled)
-                Modifier.padding(start = 2.dp, end = 2.dp).fillMaxSize().pullRefresh(pullRefreshState)
-            else
-                Modifier.padding(start = 2.dp, end = 2.dp).fillMaxSize(),
+            modifier = Modifier.padding(start = 2.dp, end = 2.dp).fillMaxSize(),
             state = listState,
         ) {
 
@@ -249,9 +252,9 @@ fun ListScreenCompact(
             }
         }
 
-        PullRefreshIndicator(
-            refreshing = false,
-            state = pullRefreshState
+        PullToRefreshContainer(
+            modifier = Modifier.align(Alignment.TopCenter).offset(y = (-30).dp),
+            state = pullToRefreshState,
         )
 
         Crossfade(listState.canScrollBackward, label = "showScrollUp") {
@@ -265,7 +268,9 @@ fun ListScreenCompact(
                             scope.launch { listState.scrollToItem(0) }
                         },
                         colors = ButtonDefaults.filledTonalButtonColors(),
-                        modifier = Modifier.padding(8.dp).alpha(0.33f)
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .alpha(0.33f)
                     ) {
                         Icon(Icons.Outlined.VerticalAlignTop, stringResource(R.string.list_scroll_to_top))
                     }
--- app/src/main/java/at/techbee/jtx/ui/list/ListScreenList.kt
+++ app/src/main/java/at/techbee/jtx/ui/list/ListScreenList.kt
@@ -19,24 +19,24 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowDropDown
 import androidx.compose.material.icons.outlined.ArrowDropUp
 import androidx.compose.material.icons.outlined.VerticalAlignTop
-import androidx.compose.material.pullrefresh.PullRefreshIndicator
-import androidx.compose.material.pullrefresh.pullRefresh
-import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -51,6 +51,7 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
@@ -79,7 +80,8 @@ import sh.calvin.reorderable.rememberReorderableLazyColumnState
 import java.util.UUID
 
 
-@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class
+)
 @Composable
 fun ListScreenList(
     groupedList: Map<String, List<ICal4ListRel>>,
@@ -133,23 +135,24 @@ fun ListScreenList(
         }
         onUpdateSortOrder(reordered)
     }
-    val pullRefreshState = rememberPullRefreshState(
-        refreshing = false,
-        onRefresh = { onSyncRequested() }
+    val pullToRefreshState = rememberPullToRefreshState(
+        enabled = { isPullRefreshEnabled }
     )
+    LaunchedEffect(pullToRefreshState.isRefreshing) {
+        if(pullToRefreshState.isRefreshing) {
+            onSyncRequested()
+            pullToRefreshState.endRefresh()
+        }
+    }
+
 
     Box(
         contentAlignment = Alignment.TopCenter,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize().nestedScroll(pullToRefreshState.nestedScrollConnection)
     ) {
 
         LazyColumn(
-            modifier = if(isPullRefreshEnabled)
-                Modifier
-                    .padding(start = 8.dp, end = 8.dp, top = 4.dp)
-                    .pullRefresh(pullRefreshState)
-                else
-                    Modifier.padding(start = 8.dp, end = 8.dp, top = 4.dp),
+            modifier = Modifier.padding(start = 8.dp, end = 8.dp, top = 4.dp),
             state = listState,
         ) {
             groupedList.forEach { (groupName, group) ->
@@ -285,9 +288,9 @@ fun ListScreenList(
             }
         }
 
-        PullRefreshIndicator(
-            refreshing = false,
-            state = pullRefreshState
+        PullToRefreshContainer(
+            modifier = Modifier.align(Alignment.TopCenter).offset(y = (-30).dp),
+            state = pullToRefreshState,
         )
 
         Crossfade(listState.canScrollBackward, label = "showScrollUp") {
