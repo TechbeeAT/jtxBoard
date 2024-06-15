86dbd38a448650085b8a64e25ed432376a0bdcd2
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2024-06-15 18:37:11 +0200

    fixed potential crash when adding subtasks and subnotes

--- app/src/main/java/at/techbee/jtx/ui/detail/DetailsScreen.kt
+++ app/src/main/java/at/techbee/jtx/ui/detail/DetailsScreen.kt
@@ -264,8 +264,10 @@ fun DetailsScreen(
             textToProcess = newSubtaskTextToProcess,
             module = Module.TODO,
             onCreate = { itemList ->
-                detailViewModel.addSubEntries(itemList, iCalObject.value?.uid!!, iCalObject.value?.collectionId!!)
-                scrollToSection.value = DetailsScreenSection.SUBTASKS
+                iCalObject.value?.let {
+                    detailViewModel.addSubEntries(itemList, it.uid, it.collectionId)
+                    scrollToSection.value = DetailsScreenSection.SUBTASKS
+                }
             },
             onDismiss = { newSubtaskTextToProcess = ""}
         )
@@ -276,8 +278,10 @@ fun DetailsScreen(
             textToProcess = newSubnoteTextToProcess,
             module = Module.NOTE,
             onCreate = { itemList ->
-                detailViewModel.addSubEntries(itemList, iCalObject.value?.uid!!, iCalObject.value?.collectionId!!)
-                scrollToSection.value = DetailsScreenSection.SUBNOTES
+                iCalObject.value?.let {
+                    detailViewModel.addSubEntries(itemList, it.uid, it.collectionId)
+                    scrollToSection.value = DetailsScreenSection.SUBNOTES
+                }
             },
             onDismiss = { newSubnoteTextToProcess = ""}
         )
