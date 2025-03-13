d9c6e0c4732ed7147a04957c87e24dfb036daaa5
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2025-03-13 09:43:47 +0100

    1757 bug full screen alarm does not show in locked screen (#1763)
    * Fixes [BUG] Full screen alarm does not show in locked screen #1757
    
    * Code layout update

--- app/src/main/java/at/techbee/jtx/AlarmFullscreenActivity.kt
+++ app/src/main/java/at/techbee/jtx/AlarmFullscreenActivity.kt
@@ -2,7 +2,9 @@ package at.techbee.jtx
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -55,8 +57,21 @@ import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.hours
 
+
 class AlarmFullscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            this.setTurnScreenOn(true)
+            this.setShowWhenLocked(true)
+            window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        } else {
+            //set flags so activity is showed when phone is off (on lock screen)
+            window.addFlags(
+                (WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                        or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                        or WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+            )
+        }
         super.onCreate(savedInstanceState)
 
         val alarmId = intent.getLongExtra(NotificationPublisher.ALARM_ID,  0L)
