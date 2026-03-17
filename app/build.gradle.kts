3c8e62f1ba4184bf7638c995eac25e4b67f32fa1
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2026-03-17 17:17:37 -0500

    AGP 9.1 upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -116,7 +116,7 @@ android {
         getByName("release") {
             isMinifyEnabled = true
             proguardFiles(
-                getDefaultProguardFile("proguard-android.txt"),
+                getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
             isShrinkResources = true
