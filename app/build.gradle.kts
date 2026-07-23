f33fb8ffbcf683fbbe5d7c4df4a7303a54a75e35
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2026-07-23 11:54:52 +0200

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "at.techbee.jtx"
         minSdk = 24
         targetSdk = 37
-        versionCode = 217020008
-        versionName = "2.17.00-beta01"      // keep -release as a suffix also for release, build flavor adds the suffix e.g. .gplay (e.g. 1.00.00-rc0.gplay)
+        versionCode = 217020009
+        versionName = "2.17.00"      // keep -release as a suffix also for release, build flavor adds the suffix e.g. .gplay (e.g. 1.00.00-rc0.gplay)
         buildConfigField("String", "versionCodename", "\"Be kind. \uD83C\uDF08\"")
         multiDexEnabled = true
         vectorDrawables.useSupportLibrary = true
